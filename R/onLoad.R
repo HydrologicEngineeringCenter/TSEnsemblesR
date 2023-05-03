a7e371244353d65e753e1963640a1ce7eb9a5cb3
eheisman <evan.a.heisman@usace.army.mil>
2023-05-03 14:25:21 -0700

    fix dependencies outside of dev environment

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,11 +1,12 @@
 .onLoad <- function(libname, pkgname){
   # TODO - Don't do this here!
-  Sys.setenv(JAVA_HOME="C:/programs/jdk-11")
+  #Sys.setenv(JAVA_HOME="C:/programs/jdk-11")
   require(rJava)
   # TODO add dss jars if needed, javaHeclib.dll
-  .jpackage(pkgname, nativeLibrary=FALSE, own.loader=TRUE,
-            parameters="-Djava.io.tmpdir=C:\\Temp\\FIRO_TSEnsembles")
-  .jaddClassPath(Sys.glob("./inst/java/*.jar"))
+  .jpackage(pkgname, nativeLibrary=FALSE, own.loader=FALSE,
+            parameters="-Djava.io.tmpdir=C:\\Temp\\FIRO_TSEnsembles",
+            morePaths=Sys.glob("./inst/java/*.jar"))
+  #.jaddClassPath(Sys.glob("./inst/java/*.jar"))
 
   # Note: use "own.loader=TRUE" with .jnew("myClass", class.loader=.rJava.class.loader) to create objects
   # need to check if nativeLibrary changes with javaHeclib requirement?
